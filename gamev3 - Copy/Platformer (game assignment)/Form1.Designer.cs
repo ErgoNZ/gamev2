b717953c49abd5f6730873f3b6326e16d7ac82f8
ErgoNZ <69284626+ErgoNZ@users.noreply.github.com>
2020-08-20 15:04:32 +1200

    Add files via upload

---
+++ gamev3 - Copy/Platformer (game assignment)/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Platformer__game_assignment_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Play = new System.Windows.Forms.Button();
+            this.Help = new System.Windows.Forms.Button();
+            this.Gametick = new System.Windows.Forms.Timer(this.components);
+            this.PnlGame = new System.Windows.Forms.Panel();
+            this.Back = new System.Windows.Forms.Button();
+            this.pngHelp = new System.Windows.Forms.PictureBox();
+            this.Level_Background = new System.Windows.Forms.PictureBox();
+            this.PlayerOne = new System.Windows.Forms.PictureBox();
+            this.Spikes = new System.Windows.Forms.PictureBox();
+            this.ground5 = new System.Windows.Forms.PictureBox();
+            this.ground4 = new System.Windows.Forms.PictureBox();
+            this.ground3 = new System.Windows.Forms.PictureBox();
+            this.ground2 = new System.Windows.Forms.PictureBox();
+            this.ground1 = new System.Windows.Forms.PictureBox();
+            this.Gravity = new System.Windows.Forms.Timer(this.components);
+            this.Door = new System.Windows.Forms.PictureBox();
+            this.PnlGame.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pngHelp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Level_Background)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Spikes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Door)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(450, 150);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(75, 23);
+            this.Play.TabIndex = 1;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Help
+            // 
+            this.Help.Location = new System.Drawing.Point(450, 263);
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(75, 23);
+            this.Help.TabIndex = 2;
+            this.Help.Text = "How to play";
+            this.Help.UseVisualStyleBackColor = true;
+            this.Help.Click += new System.EventHandler(this.Help_Click);
+            // 
+            // Gametick
+            // 
+            this.Gametick.Enabled = true;
+            this.Gametick.Interval = 25;
+            this.Gametick.Tick += new System.EventHandler(this.Gametick_Tick);
+            // 
+            // PnlGame
+            // 
+            this.PnlGame.BackColor = System.Drawing.Color.Aqua;
+            this.PnlGame.Controls.Add(this.Door);
+            this.PnlGame.Controls.Add(this.Back);
+            this.PnlGame.Controls.Add(this.pngHelp);
+            this.PnlGame.Controls.Add(this.Level_Background);
+            this.PnlGame.Controls.Add(this.PlayerOne);
+            this.PnlGame.Controls.Add(this.Spikes);
+            this.PnlGame.Controls.Add(this.ground5);
+            this.PnlGame.Controls.Add(this.ground4);
+            this.PnlGame.Controls.Add(this.ground3);
+            this.PnlGame.Controls.Add(this.ground2);
+            this.PnlGame.Controls.Add(this.ground1);
+            this.PnlGame.Location = new System.Drawing.Point(-7, 0);
+            this.PnlGame.Margin = new System.Windows.Forms.Padding(2);
+            this.PnlGame.Name = "PnlGame";
+            this.PnlGame.Size = new System.Drawing.Size(993, 463);
+            this.PnlGame.TabIndex = 6;
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(426, 429);
+            this.Back.Margin = new System.Windows.Forms.Padding(2);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(135, 19);
+            this.Back.TabIndex = 8;
+            this.Back.Text = "Back to main menu";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // pngHelp
+            // 
+            this.pngHelp.BackgroundImage = global::Platformer__game_assignment_.Properties.Resources.help;
+            this.pngHelp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pngHelp.Location = new System.Drawing.Point(6, -6);
+            this.pngHelp.Margin = new System.Windows.Forms.Padding(2);
+            this.pngHelp.Name = "pngHelp";
+            this.pngHelp.Size = new System.Drawing.Size(987, 466);
+            this.pngHelp.TabIndex = 7;
+            this.pngHelp.TabStop = false;
+            // 
+            // Level_Background
+            // 
+            this.Level_Background.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Level_Background.BackgroundImage")));
+            this.Level_Background.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Level_Background.Location = new System.Drawing.Point(4, -3);
+            this.Level_Background.Name = "Level_Background";
+            this.Level_Background.Size = new System.Drawing.Size(1000, 463);
+            this.Level_Background.TabIndex = 0;
+            this.Level_Background.TabStop = false;
+            // 
+            // PlayerOne
+            // 
+            this.PlayerOne.BackgroundImage = global::Platformer__game_assignment_.Properties.Resources.Test_Player;
+            this.PlayerOne.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.PlayerOne.Location = new System.Drawing.Point(19, 398);
+            this.PlayerOne.Name = "PlayerOne";
+            this.PlayerOne.Size = new System.Drawing.Size(36, 34);
+            this.PlayerOne.TabIndex = 1;
+            this.PlayerOne.TabStop = false;
+            // 
+            // Spikes
+            // 
+            this.Spikes.BackColor = System.Drawing.Color.Aqua;
+            this.Spikes.Image = global::Platformer__game_assignment_.Properties.Resources.Spikes;
+            this.Spikes.Location = new System.Drawing.Point(688, 142);
+            this.Spikes.Margin = new System.Windows.Forms.Padding(2);
+            this.Spikes.Name = "Spikes";
+            this.Spikes.Size = new System.Drawing.Size(39, 32);
+            this.Spikes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Spikes.TabIndex = 7;
+            this.Spikes.TabStop = false;
+            // 
+            // ground5
+            // 
+            this.ground5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ground5.BackgroundImage")));
+            this.ground5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ground5.Location = new System.Drawing.Point(559, 174);
+            this.ground5.Name = "ground5";
+            this.ground5.Size = new System.Drawing.Size(431, 35);
+            this.ground5.TabIndex = 6;
+            this.ground5.TabStop = false;
+            this.ground5.Tag = "Platform";
+            // 
+            // ground4
+            // 
+            this.ground4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ground4.BackgroundImage")));
+            this.ground4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ground4.Location = new System.Drawing.Point(375, 228);
+            this.ground4.Name = "ground4";
+            this.ground4.Size = new System.Drawing.Size(77, 35);
+            this.ground4.TabIndex = 5;
+            this.ground4.TabStop = false;
+            this.ground4.Tag = "Platform";
+            // 
+            // ground3
+            // 
+            this.ground3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ground3.BackgroundImage")));
+            this.ground3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ground3.Location = new System.Drawing.Point(262, 304);
+            this.ground3.Name = "ground3";
+            this.ground3.Size = new System.Drawing.Size(77, 35);
+            this.ground3.TabIndex = 4;
+            this.ground3.TabStop = false;
+            this.ground3.Tag = "Platform";
+            // 
+            // ground2
+            // 
+            this.ground2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ground2.BackgroundImage")));
+            this.ground2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ground2.Location = new System.Drawing.Point(75, 359);
+            this.ground2.Name = "ground2";
+            this.ground2.Size = new System.Drawing.Size(96, 35);
+            this.ground2.TabIndex = 3;
+            this.ground2.TabStop = false;
+            this.ground2.Tag = "Platform";
+            // 
+            // ground1
+            // 
+            this.ground1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ground1.BackgroundImage")));
+            this.ground1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ground1.Location = new System.Drawing.Point(3, 429);
+            this.ground1.Name = "ground1";
+            this.ground1.Size = new System.Drawing.Size(990, 50);
+            this.ground1.TabIndex = 2;
+            this.ground1.TabStop = false;
+            this.ground1.Tag = "Platform";
+            // 
+            // Gravity
+            // 
+            this.Gravity.Enabled = true;
+            this.Gravity.Interval = 25;
+            this.Gravity.Tick += new System.EventHandler(this.Gravity_Tick);
+            // 
+            // Door
+            // 
+            this.Door.BackColor = System.Drawing.Color.LightBlue;
+            this.Door.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Door.BackgroundImage")));
+            this.Door.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Door.Location = new System.Drawing.Point(896, 82);
+            this.Door.Name = "Door";
+            this.Door.Size = new System.Drawing.Size(52, 127);
+            this.Door.TabIndex = 9;
+            this.Door.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.Help);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.PnlGame);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.PnlGame.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pngHelp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Level_Background)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Spikes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Door)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button Help;
+        private System.Windows.Forms.Timer Gametick;
+        private System.Windows.Forms.PictureBox Level_Background;
+        private System.Windows.Forms.Panel PnlGame;
+        private System.Windows.Forms.PictureBox PlayerOne;
+        private System.Windows.Forms.PictureBox ground1;
+        private System.Windows.Forms.Timer Gravity;
+        private System.Windows.Forms.PictureBox ground2;
+        private System.Windows.Forms.PictureBox ground3;
+        private System.Windows.Forms.PictureBox ground4;
+        private System.Windows.Forms.PictureBox ground5;
+        private System.Windows.Forms.PictureBox Spikes;
+        private System.Windows.Forms.PictureBox pngHelp;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.PictureBox Door;
+    }
+}
+
---
+++ gamev3 - Copy/Platformer (game assignment)/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Platformer__game_assignment_
+{
+    public partial class Form1 : Form
+    {
+        // all int's and bool's for game to work
+        bool left = false;
+        bool right = false;
+        bool space = false;
+        int Ymovement;
+        int Jump = 0;
+        int level = 0;
+        bool invisable = false;
+        bool invisable1 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //This Loads on bootup
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Level_Background.Show();
+            Play.Show();
+            Help.Show();
+            pngHelp.Hide();
+            Back.Hide();
+        }
+
+        //Basic menu things
+        public void Play_Click(object sender, EventArgs e)
+        {
+             if (invisable1==false)
+            {
+                Level_Background.Hide();
+                Play.Hide();
+                Help.Hide();
+                level = 1;
+                invisable1 = true;
+            }
+        }
+
+        private void Gametick_Tick(object sender, EventArgs e)
+        {
+            //Setting up each level when the int "level" is increased by 
+            //Or the level loading
+            if (level==0)
+            {
+                ground1.Hide();
+                ground2.Hide();
+                ground3.Hide();
+                ground4.Hide();
+                ground5.Hide();
+                Spikes.Hide();
+                Door.Hide();               
+            }
+            else
+            {
+                if (level==1)
+                {
+                    ground1.Show();
+                    ground2.Show();
+                    ground3.Show();
+                    ground4.Show();
+                    ground5.Show();
+                    Spikes.Show();
+                    Door.Show();
+                }
+                else
+                {
+                    if (level == 2)
+                    {                        
+                        ground2.Hide();
+                        ground3.Hide();
+                        ground4.Hide();
+                        ground5.Hide();
+                        Spikes.Hide();
+                        //This makes the door move to the new location
+                        Door.Hide();
+                    }
+                    else
+                    {
+                        if (level == 3)
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+            }
+
+            //movement (I got some help on this)
+            if (space == true)
+            {
+                if (Jump == 0)
+                {
+                    Jump = 1;
+                    Ymovement += -12;
+                }
+            }
+            if (left == true)
+            {
+                if (PlayerOne.Location.X < -10)
+                {
+                    PlayerOne.Left = 1000;
+                }
+                else
+                {
+                    PlayerOne.Left -= 5;
+                }
+            }
+            if (right == true)
+            {
+                if (PlayerOne.Location.X > PnlGame.Width )
+                {
+                    PlayerOne.Left = -10;
+                }
+                else
+                {
+                    PlayerOne.Left += 5;
+                }
+            }
+            if (PlayerOne.Bounds.IntersectsWith(Spikes.Bounds))
+            {
+                PlayerOne.Left = PnlGame.Width + 70;
+                PlayerOne.Top = PnlGame.Height - 100;
+                //debuging info
+                Console.WriteLine(PlayerOne.Top);
+                Console.WriteLine(PnlGame.Top);
+            }
+        }
+        private void Gravity_Tick(object sender, EventArgs e)
+        {
+            //Collistion detection (Note: this took me way longer than it should have)
+            PlayerOne.Top += Ymovement;
+            if (PlayerOne.Bounds.IntersectsWith(ground1.Bounds) == true)
+            //this part was easy but finding collistion for all platforms easily didnt happen so we end up with this mess of "if" and "else"
+            {
+                Ymovement = 0;
+                Jump = 0;
+                PlayerOne.Top = ground1.Top - 34;
+            }
+
+            else
+            {
+                //figured out a slow and painful way of dectecting for all platforms for above and below so people cant shoot right through them (I should have dont this from the start)
+                if (PlayerOne.Bounds.IntersectsWith(ground2.Bounds))
+                {
+                    //I used console to write lines of the pos of player and ground's top to figure out the Y cords for the above/below decetection
+                    if (PlayerOne.Location.Y < 350)
+                    {
+                        PlayerOne.Top = ground2.Top - 34;
+                        Ymovement = 0;
+                        Jump = 0;
+                    }
+                    else
+                    {
+                        PlayerOne.Top = ground2.Top + 36;
+                        Ymovement = 0;
+                    }
+                    //this code ^^^^^^^ is the base set up for all collistion   
+                }
+                if (PlayerOne.Bounds.IntersectsWith(ground3.Bounds))
+                {
+                    if (PlayerOne.Location.Y < 305)
+                    {
+                        PlayerOne.Top = ground3.Top - 34;
+                        Ymovement = 0;
+                        Jump = 0;
+                    }
+                    else
+                    {
+                        PlayerOne.Top = ground3.Top + 36;
+                        Ymovement = 0;
+                    }
+                }
+                else                
+                    if (PlayerOne.Bounds.IntersectsWith(ground4.Bounds))
+                {
+                    if (PlayerOne.Location.Y < 230)
+                    {
+                        PlayerOne.Top = ground4.Top - 34;
+                        Ymovement = 0;
+                        Jump = 0;
+                    }
+                    else
+                    {
+                        PlayerOne.Top = ground4.Top + 36;
+                        Ymovement = 0;
+                    }
+                }
+                else
+                    if (PlayerOne.Bounds.IntersectsWith(ground5.Bounds))
+                {
+                    if (PlayerOne.Location.Y < 174)
+                    {
+                        PlayerOne.Top = ground5.Top - 34;
+                        Ymovement = 0;
+                        Jump = 0;
+                    }
+                    else
+                    {
+                        PlayerOne.Top = ground4.Top -10;
+                        Ymovement = 0;
+                    }
+                }
+                else
+                {
+                    if (PlayerOne.Bounds.IntersectsWith(ground1.Bounds))
+                    {
+                        Ymovement = 0;
+                        Jump = 0;
+                        PlayerOne.Top = ground1.Top - 34;
+                    }
+                    else
+                    {
+                        Ymovement += 1;
+                        if (Ymovement > 20)
+                        {
+                            Ymovement = 20;
+                        }
+                    }
+                }
+    }
+}
+
+        //key dectection
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { left = true; }
+            if (e.KeyData == Keys.Right){ right = true; }
+            if (e.KeyData == Keys.Space) { space = true; }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { left = false; }
+            if (e.KeyData == Keys.Right) { right = false; }
+            if (e.KeyData == Keys.Space) { space = false; }
+        }
+
+        // this is the code for the help menu which lists controls and basic mechanics
+        private void Help_Click(object sender, EventArgs e)
+        {
+            if (invisable1 == false)
+            {
+                Play.Hide();
+                Help.Hide();
+                Level_Background.Hide();
+                pngHelp.Show();
+                Back.Show();
+                invisable = false;
+            }
+        }      
+        //Back to main menu from help screen
+        private void Back_Click(object sender, EventArgs e)
+        {
+            if (invisable == false)
+            {
+                Level_Background.Show();
+                Play.Show();
+                Help.Show();
+                pngHelp.Hide();
+                Back.Hide();
+                invisable = true;
+            }
+        }
+
+    }
+
+}
